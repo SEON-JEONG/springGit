7b042272662a788a6cdfa52c6de463cce187991d
SEON-JEONG <percyknow@gmail.com>
2024-11-22 12:02:43 +0900

    집에서 소스수정 완료!!

--- demo/src/main/java/com/example/demo/main_controller.java
+++ demo/src/main/java/com/example/demo/main_controller.java
@@ -1,7 +1,16 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
 
 public class main_controller {
 
 	
+}
+
+// 컨트롤러 외부 클래스로 매핑!!
+class abc{
+	@GetMapping("/top.do")
+	public String top() {
+		return null;
+	}
 }
